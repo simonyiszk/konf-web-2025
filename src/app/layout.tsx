e192eb0114666b7aad77a15e133937854e3d2fee
Gergő Bujdosó <bujgergo@gmail.com>
2024-12-03 00:02:05 +0100

    google site verif

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,10 @@ export const metadata: Metadata = {
     "Magyarország legnagyobb egyetemi hallgatók által szervezett éves technológiai konferenciája.",
   keywords:
     "Simonyi Konferencia 2025, technológiai konferencia, egyetemi rendezvény, hallgatók, hallgatók szervezése, Simonyi Károly Szakkollégium, BME-VIK, innováció, digitalizáció, műszaki fejlesztések, tudományos esemény, inspiráló előadások, szakmai workshopok, Magyarország eseményei, fiatal tehetségek, digitális megoldások, jövő technológiái, iparági trendek, tudásátadás, innovatív gondolkodás, egyetemi közösség, kreatív technológia, networking lehetőségek, szakmai előadók, technológiai innovációk, informatikai fejlődés, egyetemi tapasztalatok, mérnöki világ, vezető szakemberek, digitális társadalom, tudományos találkozó",
+  other: {
+    "google-site-verification": "IEdi66LPaMH1EMd-iMpGGl13QKlQTBIFTNL7TEv2tY8",
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
