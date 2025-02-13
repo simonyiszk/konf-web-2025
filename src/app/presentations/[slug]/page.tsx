543af566e631c12e19966adc8b789d4cab04c7f7
Gergő Bujdosó <bujgergo@gmail.com>
2025-02-13 09:51:00 +0100

    fixed site generation

--- src/app/presentations/[slug]/page.tsx
+++ src/app/presentations/[slug]/page.tsx
@@ -1,9 +1,9 @@
-import { Metadata, ResolvingMetadata } from 'next';
-import { notFound } from 'next/navigation';
+import { Metadata, ResolvingMetadata } from "next";
+import { notFound } from "next/navigation";
 
-import Presentation from '@/components/presentation/Presentation';
-import { getIndexData } from '@/models/get-index-data';
-import slugify from '@/utils/slugify';
+import Presentation from "@/components/presentation/Presentation";
+import { getIndexData } from "@/models/get-index-data";
+import slugify from "@/utils/slugify";
 
 export async function generateStaticParams() {
   const data = await getIndexData();
@@ -16,19 +16,27 @@ export async function generateStaticParams() {
 }
 
 type Props = {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 };
 
-export async function generateMetadata({ params: { slug } }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+export async function generateMetadata(
+  props: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
   const data = await getIndexData();
-  const presentation = data?.presentations.find((p) => slugify(p.title) === slug);
+  const slug = (await props.params).slug;
+  const presentation = data?.presentations.find(
+    (p) => slugify(p.title) === slug
+  );
 
   return {
     title: presentation?.title,
     description: `${presentation?.presenter.name} "${presentation?.title}" című előadása a XXI. Simonyi Konferencián`,
-    keywords: `${(await parent).keywords}, ${presentation?.presenter.name}${presentation?.title
-      .split(' ')
-      .reduce((prev, curr) => `${prev}, ${curr}`, '')}`,
+    keywords: `${(await parent).keywords}, ${
+      presentation?.presenter.name
+    }${presentation?.title
+      .split(" ")
+      .reduce((prev, curr) => `${prev}, ${curr}`, "")}`,
   };
 }
 
@@ -37,14 +45,18 @@ const getPresentationBySlug = async (slug: string) => {
   return data?.presentations.find((p) => slugify(p.title) === slug);
 };
 
-export default async function PresentationBySlug({ params }: { params: { slug: string } }) {
-  const presentation = await getPresentationBySlug(params.slug);
+export default async function PresentationBySlug({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const presentation = await getPresentationBySlug((await params).slug);
   if (!presentation) {
     notFound();
   }
 
   return (
-    <div className='mt-10 px-4 sm:px-6 xl:px-0 overflow-hidden'>
+    <div className="mt-10 px-4 sm:px-6 xl:px-0 overflow-hidden">
       <Presentation presentation={presentation} />
     </div>
   );
