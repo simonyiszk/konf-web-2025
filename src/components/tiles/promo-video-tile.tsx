295490191ecea6cc8ce8e637dd251f0d5e258d35
Miklos Daniel <daniel.miklos0@gmail.com>
2025-02-24 19:23:32 +0100

    adjust video tile width

--- src/components/tiles/promo-video-tile.tsx
+++ src/components/tiles/promo-video-tile.tsx
@@ -1,6 +1,7 @@
 import { PromoVideoData } from "@/models/models";
 
 import { YoutubeVideo } from "./youtube-video";
+import { ContentLayout } from "../layout";
 
 type Props = {
   data: PromoVideoData;
@@ -9,12 +10,14 @@ type Props = {
 export function PromoVideoTile({ data: { sectionTitle, youtubeUrl } }: Props) {
   return (
     <div className="flex flex-col items-center p-8 w-full px-auto relative z-10">
-      <h1 className="xs:text-4xl sm:text-6xl text-xl font-bold text-center">
-        {sectionTitle}
-      </h1>
-      <div className="mt-8 gap-4 xl:w-[900px] w-4/5">
-        <YoutubeVideo title={sectionTitle} url={youtubeUrl} />
-      </div>
+      <ContentLayout>
+        <h1 className="xs:text-4xl sm:text-6xl text-xl font-bold text-center">
+          {sectionTitle}
+        </h1>
+        <div className="mt-8 gap-4 xl:w-[900px] w-4/5">
+          <YoutubeVideo title={sectionTitle} url={youtubeUrl} />
+        </div>
+      </ContentLayout>
     </div>
   );
 }
